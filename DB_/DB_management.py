a0bd194bc1d8d5bef6504b479e87c7aa3baa3e61
PotapovAnton-gif <antpotapov2019@gmail.com>
2022-03-02 22:28:47 +0300

    Fix bot_hack: DB_managemment fixing some errors about access for keys

--- DB_/DB_management.py
+++ DB_/DB_management.py
@@ -1,34 +1,32 @@
 import pymongo
-from pymongo import MongoCient as MC
 from typing import Dict
 
 class DB_management():
     
     #connecting to MongoDB by setted URL
-    def __init__(self, url):
-        self.client = MC(url)
-    
-    def connect_to_DB(self, DB_name: str) -> None:
+    def __init__(self, url, DB_name: str):
+        self.client = pymongo.MongoClient(url)
         self.current_DB = self.client[DB_name]
+
 
     def connect_to_a_project(self, project_name: str) -> None:
         self.current_project = self.current_DB[project_name]
 
-    def write_a_message(self, message: Dict, importance_marker: str) -> None:
-        cur_dict = {"message_id" : message["message_id"],
-                    "chat_id" : message["chat"]["id"],
-                    "user_id" : message["from"]["id"],
-                    "first_name" : message["from"]["first_name"], 
-                    "user_name" : message["from"]["user_name"],  
-                    "date" : message["date"],
-                    "importance_marker" : message["importance_marker"], # докнинь, как лучше сделать: это поле передовать через сам метод, или через поле в словаре message 
-                    "message_text" : message["text"],
-                    "message_type" : message["message_type"],
-                    "content_id" :message["content_id"]
+    def push_into_DB(self, message: Dict, importance_marker: str) -> None:
+        cur_dict = {"message_id": message.get("message_id", 0),
+                    "chat_id": message.get("chat", 0).get("id", 0),
+                    "user_id": message.get("from", 0).get("id", 0),
+                    "first_name": message.get("from", 0).get("first_name", 0), 
+                    "user_name": message.get("from", 0).get("user_name", 0),  
+                    "date": message.get("date", 0),
+                    "importance_marker": message.get("importance_marker", 0), # докнинь, как лучше сделать: это поле передовать через сам метод, или через поле в словаре message 
+                    "message_text": message.get("text", 0),
+                    "message_type": message.get("message_type", 0),
+                    "content_id" :message.get("content_id", 0) # пока что не парсил сам контент, просто посмотри на форму записи, и поправль меня)
                     }
         self.current_project.insert(cur_dict)
 
-    def select_from_DB_search_by(self, filtr):
+    def select_from_DB_search_by(self, filtr: Dict): #filtr = {"count" : int a = 0, "date" : int a = 0, "importance_marker" : "xxx"}
         pass
 
 
