e74359ca7f84a0991792c3e88f7273be25c30953
PotapovAnton-gif <antpotapov2019@gmail.com>
2022-03-03 17:57:08 +0300

    Fix bot_hack: database_MG added a try - except contruction, base inquiry

--- DB_/DB_management.py
+++ DB_/DB_management.py
@@ -1,7 +1,7 @@
 import pymongo
 from typing import Dict
 
-class DB_management():
+class DB_management:
     
     #connecting to MongoDB by setted URL
     def __init__(self, url, DB_name: str):
@@ -13,24 +13,42 @@ class DB_management():
         self.current_project = self.current_DB[project_name]
 
     def push_into_DB(self, message: Dict, importance_marker: str) -> None:
-        cur_dict = {"message_id": message.get("message_id", 0),
-                    "chat_id": message.get("chat", 0).get("id", 0),
-                    "user_id": message.get("from", 0).get("id", 0),
-                    "first_name": message.get("from", 0).get("first_name", 0), 
-                    "user_name": message.get("from", 0).get("user_name", 0),  
-                    "date": message.get("date", 0),
-                    "importance_marker": message.get("importance_marker", 0), # докнинь, как лучше сделать: это поле передовать через сам метод, или через поле в словаре message 
-                    "message_text": message.get("text", 0),
-                    "message_type": message.get("message_type", 0),
-                    "content_id" :message.get("content_id", 0) # пока что не парсил сам контент, просто посмотри на форму записи, и поправль меня)
-                    }
-        self.current_project.insert(cur_dict)
+        try:
+            cur_dict = {"message_id": message["message_id"],
+                        "chat_id": message["chat"]["id"],
+                        "user_id": message["from"]["id"],
+                        "first_name": message["from"]["first_name"],
+                        "user_name": message["from"]["user_name"],
+                        "date": message["date"],
+                        # докнинь, как лучше сделать: это поле передовать через сам метод, или через поле в словаре message
+                        "importance_marker": message.get("importance_marker", "green"),
+                        "message_text": message["text"],
+                        "media_group_id": message.get(["media_group_id"]),
+                        "message_type": message.get("message_type"),
+                        "content_id": {"photo": [i_d["file_id"] for i_d in message.get("photo")],
+                                       "audio": [i_d["file_id"] for i_d in message.get("audio")],
+                                       "video": [i_d["file_id"] for i_d in message.get("video")],
+                                       "voice": [i_d["file_id"] for i_d in message.get("voice")],
+                                       "document": [i_d["file_id"] for i_d in message.get("document")],
+                                       "location": message.get("location")
+                                       }
+                        }
+            self.current_project.insert_one(cur_dict)
+        except:
+            raise KeyError(cur_dict)
 
-    def select_from_DB_search_by(self, filtr: Dict): #filtr = {"count" : int a = 0, "date" : int a = 0, "importance_marker" : "xxx"}
+    def select_from_DB_search_by(self, filtr: Dict): 
+    #filtr = {"count" : int a = None, how : int a = {-1, 0, 1} - ищем меньше исходной даты(-1), больше(1) или равне(0); в случае, когда ищем вообще все записи по проекту и дата равна 0,передавать 1 "date" : int a = 0, "importance_marker" : "xxx", "sort" : int a = {-1, 1}}
+        
+        if filtr["how"] == 0:
+            return self.current_project.find({"date": filtr["date"], "importance_marker": filtr["importance_marker"] }).sotr({$natural: filtr["sort"]}).limit(filtr["limit"])
+        elif filtr["hom"] == 1:
+            return self.current_project.find({"date":{$gt: filtr["date"]}, "importance_marker": filtr["importance_marker"] }).sotr({$natural: filtr["sort"]}).limit(filtr["limit"])
+        elif filtr["how"] == -1:
+            return self.current_project.find({"date":{$lt: filtr["date"]}, "importance_marker": filtr["importance_marker"] }).sotr({$natural: filtr["sort"]}).limit(filtr["limit"])
+    
+    def select_from_DB_search_by_range(self, filtr: Dict):
         pass
-
-
-
 
 
 
