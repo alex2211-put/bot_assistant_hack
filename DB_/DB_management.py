f5229237351de77785bb1fcb214e27b7b0d51373
PotapovAnton-gif <antpotapov2019@gmail.com>
2022-03-17 17:53:13 +0300

    Fix bot_hack: database MG attempt to fix

--- DB_/DB_management.py
+++ DB_/DB_management.py
@@ -13,21 +13,19 @@ class DB_management:
         self.current_project = self.current_DB[project_name]
 
     def __get_id(self, message: Dict) -> List:
-        types_of_content= ["vioce", "photo", "video", "audio", "document", "location"]
+        types_of_content = ["vioce", "photo",
+                            "video", "audio", "document", "location"]
         for i in types_of_content:
             if message.get(i) is not None:
                 content_type = i
                 break
         msg_id = message.get(content_type)["id"]
         return [content_type, msg_id]
-    
-        
-        return [None, None]
 
     def push_into_DB(self, message: Dict, importance_marker: str, message_type) -> None:
 
         content_type, content_id = self.__get_id(message)
-        
+
         cur_dict = {"message_id": message["message_id"],
                     "chat_id": message["chat"]["id"],
                     "user_id": message["from"]["id"],
@@ -43,14 +41,18 @@ class DB_management:
                     "content_id": content_id
                     }
         self.current_project.insert_one(cur_dict)
-    
-    
 
-    def select_from_DB_search_by(self, count: int = 0, how: str = 'eq', date: int = 0, importance_marker: str = 'green', sort: int = 1) -> Dict:
+    def select_from_DB_search_by(self,
+                                 count: int = 0,
+                                 how: str = 'eq',
+                                 date: int = 0,
+                                 importance_marker: str = 'green',
+                                 sort: int = 1
+                                 ) -> Dict:
         '''
         Discribtion param filtr
         :count : int a = 0 by default
-        :how = {'$gt', 'eq', '$lt'} -меньше даты(lt), больше(gt) или равна(0), в случае, когда ищем все записи по проекту, передавать 1 
+        :how = {'$gt', 'eq', '$lt'} -меньше даты(lt), больше(gt) или равна(0)
         :date : int a = 0 by default
         :importance_marker : "xxx"
         :sort : int a = {-1, 1}}
@@ -58,17 +60,27 @@ class DB_management:
         '''
         if count == 0:
             if how == 'eq':
-                return self.current_project.find({"date": date, "importance_marker": importance_marker}).sort({'date': sort})
+                return self.current_project.find({"date": date,
+                                                  "importance_marker": importance_marker}).sort({'date': sort})
             else:
-                return self.current_project.find({"date": {how: date}, "importance_marker": importance_marker}).sort({'date': sort})
+                return self.current_project.find({"date": {how: date},
+                                                  "importance_marker": importance_marker}).sort({'date': sort})
 
         else:
             if how == 'eq':
-                return self.current_project.find({"date": date, "importance_marker": importance_marker}).sort({'date': sort}).limit(count)
+                return self.current_project.find({"date": date,
+                                                  "importance_marker": importance_marker}).sort({'date': sort}).limit(count)
             else:
-                return self.current_project.find({"date": {how: date}, "importance_marker": importance_marker}).sort({'date': sort}).limit(count)
+                return self.current_project.find({"date": {how: date},
+                                                  "importance_marker": importance_marker}).sort({'date': sort}).limit(count)
 
-    def select_from_DB_search_by_range(self, count: int, date_start: int, date_finish: int, importance_marker: str = 'green', sort: int = 1) -> Dict:
+    def select_from_DB_search_by_range(self,
+                                       count: int,
+                                       date_start: int,
+                                       date_finish: int,
+                                       importance_marker: str = 'green',
+                                       sort: int = 1
+                                       ) -> Dict:
         '''
          Discribtion param filtr
         :count : int a = 0 by default
@@ -79,9 +91,8 @@ class DB_management:
         :return : dict
         '''
         if count == 0:
-
-           return self.current_project.find({"date": {"$gt": date_start, "$lt": date_finish}, "importance_marker": importance_marker}).sort({'date': sort})
-
+           return self.current_project.find({"date": {"$gt": date_start, "$lt": date_finish},
+                                             "importance_marker": importance_marker}).sort({'date': sort})
         else:
-
-            return self.current_project.find({"date": {"$gt": date_start, "$lt": date_finish}, "importance_marker": importance_marker}).sort({'date': sort}).limit(count)
+            return self.current_project.find({"date": {"$gt": date_start, "$lt": date_finish},
+                                              "importance_marker": importance_marker}).sort({'date': sort}).limit(count)
