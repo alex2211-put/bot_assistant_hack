ca192933d1982a0b0eb8ffee7543b6518b76d784
PotapovAnton-gif <antpotapov2019@gmail.com>
2022-03-17 13:56:09 +0300

    Fix bot_hack: database MG removed try - exception block

--- DB_/DB_management.py
+++ DB_/DB_management.py
@@ -18,34 +18,33 @@ class DB_management:
             if message.get(i) is not None:
                 content_type = i
                 break
-        try:
-            msg_id = message.get(content_type)["id"]
-            return [content_type, msg_id]
-        except:
-            raise KeyError()
+        msg_id = message.get(content_type)["id"]
+        return [content_type, msg_id]
+    
+        
         return [None, None]
 
     def push_into_DB(self, message: Dict, importance_marker: str, message_type) -> None:
 
         content_type, content_id = self.__get_id(message)
-        try:
-            cur_dict = {"message_id": message["message_id"],
-                        "chat_id": message["chat"]["id"],
-                        "user_id": message["from"]["id"],
-                        "first_name": message["from"].get("first_name"),
-                        "last_name": message["from"].get("last_name"),
-                        "user_name": message["from"]["user_name"],
-                        "date": message["date"],
-                        "importance_marker": importance_marker,
-                        "message_text": message["text"],
-                        "media_group_id": message.get(["media_group_id"]),
-                        "message_type": message_type,
-                        "content_type": content_type,
-                        "content_id": content_id
-                        }
-            self.current_project.insert_one(cur_dict)
-        except:
-            raise KeyError(cur_dict)
+        
+        cur_dict = {"message_id": message["message_id"],
+                    "chat_id": message["chat"]["id"],
+                    "user_id": message["from"]["id"],
+                    "first_name": message["from"].get("first_name"),
+                    "last_name": message["from"].get("last_name"),
+                    "user_name": message["from"]["user_name"],
+                    "date": message["date"],
+                    "importance_marker": importance_marker,
+                    "message_text": message["text"],
+                    "media_group_id": message.get(["media_group_id"]),
+                    "message_type": message_type,
+                    "content_type": content_type,
+                    "content_id": content_id
+                    }
+        self.current_project.insert_one(cur_dict)
+    
+    
 
     def select_from_DB_search_by(self, count: int = 0, how: str = 'eq', date: int = 0, importance_marker: str = 'green', sort: int = 1) -> Dict:
         '''
