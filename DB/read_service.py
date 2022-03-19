9f54ef07b036cc03240ad57871428e14438cd955
PotapovAnton-gif <antpotapov2019@gmail.com>
2022-03-19 10:56:21 +0300

    Fix bot_hack: database MG removed main in read_service

--- DB/read_service.py
+++ DB/read_service.py
@@ -5,6 +5,3 @@ def get_mongo_url():
     with open('service.yaml', encoding='utf-8') as file:
         parameters = yaml.safe_load(file)
         return parameters.get('url')
-
-if __name__ == '__main__':
-    print(get_url())
